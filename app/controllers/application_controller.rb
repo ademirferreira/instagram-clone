b5b7de866f7e750f5aad44ae387a7d6af64d6c0a
Ademir Ferreira <ademir.ae86@gmail.com>
2020-04-02 21:33:57 -0300

    Devise configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitter_parameters, if: :devise_controller?
+
+  layout :layout_by_resource
+
+  private
+
+  def layout_by_resource
+    'authentication' if devise_controller?
+  end
+
+  def configure_permitter_parameters
+    devise_parameter_sanizer.permite :sign_up, keys: %i[name username]
+  end
 end
