66d244045a330d8b29dac308a4959d31f01dc1d8
Ademir Ferreira <ademir.ae86@gmail.com>
2020-04-03 20:59:35 -0300

    Create upload photos and avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.required(:user).permit(:name)
+    params.required(:user).permit(:name, :avatar)
   end
 
   private
