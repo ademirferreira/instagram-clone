45d134c94053e160ef3856c0adfdb9f95db74759
Ademir Ferreira <ademir.ae86@gmail.com>
2020-04-03 15:26:13 -0300

    Add route resources for user

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
 
   devise_for :users, only: %i[sessions registrations passwords]
   resources :posts, only: %i[index show new create]
+  resources :users, only: %i[show], param: :username
 end
