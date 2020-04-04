501cd3a79a600942b40f86ab79d4e8100714a136
Ademir Ferreira <ademir.ae86@gmail.com>
2020-04-03 21:07:35 -0300

    Create model and migrations to like

---
+++ app/models/like.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class Like < ApplicationRecord
+  belongs_to :post
+  belongs_to :user
+
+  validates :post_id, uniqueness: { scope: user_id, message: 'post já está curtido' }
+end
