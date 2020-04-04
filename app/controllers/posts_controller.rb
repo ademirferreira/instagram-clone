5ca0fa69ec076a385c85d211e535ccf011f87e22
Ademir Ferreira <ademir.ae86@gmail.com>
2020-04-04 00:08:39 -0300

    Create channel posts using action cable

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params.merge(created_by: current_user))
 
     if @post.save
+      PostChannel.broadcast_to 'post_channel', post_created: render_to_string(partial: @post)
       redirect_to @post, notice: 'Post foi criado com sucesso.'
     else
       flash.now[:alert] = @post.errors.full_messages.to_sentence
